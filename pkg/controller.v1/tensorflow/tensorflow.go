3f846bba04fe067c4a523c0483c749842f119a42
Yi Chen <github@chenyicn.net>
2025-09-24 19:04:06 +0800

    Bump Golang from 1.18 to 1.23.12
    Signed-off-by: Yi Chen <github@chenyicn.net>

--- pkg/controller.v1/tensorflow/tensorflow.go
+++ pkg/controller.v1/tensorflow/tensorflow.go
@@ -59,17 +59,18 @@ type TaskSpec struct {
 }
 
 // genTFConfig will generate the environment variable TF_CONFIG
-// {
-//     "cluster": {
-//         "ps": ["ps1:2222", "ps2:2222"],
-//         "worker": ["worker1:2222", "worker2:2222", "worker3:2222"]
-//     },
-//     "task": {
-//         "type": "ps",
-//         "index": 1
-//         },
-//     }
-// }
+//
+//	{
+//	    "cluster": {
+//	        "ps": ["ps1:2222", "ps2:2222"],
+//	        "worker": ["worker1:2222", "worker2:2222", "worker3:2222"]
+//	    },
+//	    "task": {
+//	        "type": "ps",
+//	        "index": 1
+//	        },
+//	    }
+//	}
 func genTFConfigJSONStr(tfjob *tfv1.TFJob, rtype, index string) (string, error) {
 	// Configure the TFCONFIG environment variable.
 	i, err := strconv.ParseInt(index, 0, 32)
